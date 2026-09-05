f6989c3bf4555bc6c4b35542c3025afe2856aa1d
Tarun Kumar <tarun.mishra592@gmail.com>
2026-09-05 18:20:34 +0530

    commit lint setup

---
+++ commitlint.config.js
@@ -0,0 +1,53 @@
+
+/**@type {import('@commitlint/types').UserConfig}*/
+
+// JIRA-123:feat
+
+const config = {
+    extends: ['@commitlint/config-conventional'],
+    plugins:[
+        {
+            rules:{
+                'jira-id-check': (parse) => {
+                    const header = parse.header
+
+                    const jiraPattern = /^PROJ-\d+:\s.+/;
+
+
+                    const isValid = jiraPattern.test(header)
+
+                    return[
+                        isValid,
+                        `Commit message should have JIRA ID\n`+
+                        `The correct formate is PROJ-123:feat: commit message.`
+                    ]
+                }
+            },
+            rules:{
+                'jira-id-check': [2, 'always'],
+
+                'type-enum':[
+                    2,
+                    'always',
+                    [
+                        'fix',      // bug fix
+                        'feat',     // new feature
+                        'chore',    // maintenance - deps update etc.
+                        'docs',     // documentation
+                        'style',    // formatting - no logic change
+                        'refactor', // code restructure
+                        'perf',     // performance improvement
+                        'test',     // tests add/update
+                        'revert',   // previous commit revert
+                        'ci',       // CI/CD changes
+                    ]
+                ],
+                'subject-case': [2, 'always', 'lower-case'],
+                'subject-empty': [2, 'never'],
+                'type-empty': [2, 'never']
+            }
+        }
+    ]
+}
+
+export default config
